224646bcd09e6bd9db50e074f1d8658c3c6182c0
PranshuPandya <72974832+PranshuPandya@users.noreply.github.com>
2021-10-02 15:30:29 +0530

    fixed the bug of scrolling the screen (#17)
    * fixed the bug of scrolling the screen
    
    and added padding to createListOfOrder()
    
    Co-authored-by: Akshay <jadhavAkshay0701@gmail.com>

--- lib/screens/MyOrderPage.dart
+++ lib/screens/MyOrderPage.dart
@@ -23,14 +23,15 @@ import 'package:food_delivery_app/resourese/auth_methods.dart';
 import 'package:food_delivery_app/resourese/firebase_helper.dart';
 import 'package:food_delivery_app/utils/universal_variables.dart';
 import 'package:food_delivery_app/widgets/orderwidget.dart';
+
 class MyOrderPage extends StatefulWidget {
   @override
   _MyOrderPageState createState() => _MyOrderPageState();
 }
 
 class _MyOrderPageState extends State<MyOrderPage> {
-  List<Request> requestList=[];
-  AuthMethods authMethods=AuthMethods();
+  List<Request> requestList = [];
+  AuthMethods authMethods = AuthMethods();
   FirebaseHelper mFirebaseHelper = FirebaseHelper();
   FirebaseUser currentUser;
 
@@ -38,8 +39,8 @@ class _MyOrderPageState extends State<MyOrderPage> {
   void initState() {
     super.initState();
     SchedulerBinding.instance.addPostFrameCallback((timeStamp) async {
-      currentUser= await authMethods.getCurrentUser();
-      mFirebaseHelper.fetchOrders(currentUser).then((List<Request> list){
+      currentUser = await authMethods.getCurrentUser();
+      mFirebaseHelper.fetchOrders(currentUser).then((List<Request> list) {
         // there are not much sync operation in myorder page, i.e didn;t made any bloc file :)
         setState(() {
           requestList = list;
@@ -51,50 +52,48 @@ class _MyOrderPageState extends State<MyOrderPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.only(top: 0.0,left: 20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 10.0,left:18.0),
-                  child: Text("My Orders",
-                    style: TextStyle(
-                      color: UniversalVariables.orangeAccentColor, 
-                      fontSize: 40.0, 
-                      fontWeight: FontWeight.bold,
-                      ),
-                  ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10.0, left: 18.0),
+              child: Text(
+                "My Orders",
+                style: TextStyle(
+                  color: UniversalVariables.orangeAccentColor,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
                 ),
-                createListOfOrder()
-              ],
+              ),
             ),
-          ),
+            Padding(
+              padding: EdgeInsets.only(top: 0.0,left: 20.0),
+              child: createListOfOrder(),
+            ),
+          ],
         ),
-    );
-  }
-
-  createListOfOrder(){
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      child: requestList.length==-1 ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-          physics: NeverScrollableScrollPhysics(),
-          scrollDirection: Axis.vertical,
-          itemCount: requestList.length,
-          itemBuilder: (_,index){
-            return OrderWidget(
-              requestList[index],
-            );
-          }
       ),
     );
   }
+
+  createListOfOrder() {
+    return requestList.length == -1
+      ? Center(child: CircularProgressIndicator())
+      : ListView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          scrollDirection: Axis.vertical,
+          itemCount: requestList.length,
+          itemBuilder: (_, index) {
+            return OrderWidget(
+              requestList[index],
+            );
+          },
+        );
+  }
 }
-
-
-
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -348,4 +348,3 @@ class _HomePageContentState extends State<HomePageContent> {
     );
   }
 }
-   
